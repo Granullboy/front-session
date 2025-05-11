13cf4c61b1b05524ab6876a0d819c9773b5f79e2
Granullboy <granulboy@gmail.com>
2025-05-12 01:41:34 +0500

    update Layout

--- keep-coins/src/components/Layout.tsx
+++ keep-coins/src/components/Layout.tsx
@@ -18,42 +18,43 @@ export const Layout = () => {
 
   return (
     <div className="min-h-screen flex flex-col bg-white text-black dark:bg-slate-900 dark:text-white transition-colors duration-300">
-        <nav className="flex gap-4 justify-end items-center p-4 bg-gray-100 dark:bg-gray-800 shadow-md">
-      <button 
-      className="px-4 py-2 bg-gray-100 text-black dark:bg-gray-800 dark:text-white rounded hover:bg-gray-200 dark:hover:bg-gray-700 transition"
-      onClick={() => navigate('/')}
-      >
-      <h3 className="text-xl font-bold">Keep Coins</h3>
-      </button>
-
-    {currentUser ? (
-      <>
-        <span className="text-black dark:text-white">
-          Welcome, {currentUser.name}!
-        </span>
+      <nav className="flex items-center justify-between p-4 bg-gray-900">
         <button 
-          className="px-4 py-2 bg-gray-100 text-black dark:bg-gray-800 dark:text-white rounded hover:bg-gray-200 dark:hover:bg-gray-700 transition"
-          onClick={() => navigate('/logout')}
+          className="cursor-pointer ml-4 md:ml-6"
+          onClick={() => navigate('/')}
         >
-          Logout
+          <h3 className="text-2xl font-bold">Keep Coins</h3>
         </button>
-      </>
-    ) : (
-      <button 
-        className="px-4 py-2 bg-gray-100 text-black dark:bg-gray-800 dark:text-white rounded hover:bg-gray-200 dark:hover:bg-gray-700 transition"
-        onClick={() => navigate('/logout')}
-        >
-        Login
-        </button>
-    )}
+        <div className="flex items-center gap-4 mr-4">{/*обнавляю стиль у layout*/ }
+          {currentUser ? (
+            <>
+              <button className="text-white bg-transparent border-none " onClick={() => navigate('/dashboard')}>Dashboard</button>
+              <button className="text-white bg-transparent border-none " onClick={() => navigate('/stats')}>Statistics</button>
+              <button className="text-white bg-transparent border-none " onClick={() => navigate('/settings')}>Settings</button>
+              <button 
+                className="text-white hover:text-gray-300 bg-transparent border-none cursor-pointer"
+                onClick={() => navigate('/logout')}
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <button 
+              className="px-4 py-2 bg-gray-100 text-black dark:bg-gray-800 dark:text-white rounded hover:bg-gray-200 dark:hover:bg-gray-700 transition"
+              onClick={() => navigate('/logout')}
+              >
+              Login
+              </button>
+          )}
 
-    <button 
-      className="px-4 py-2 bg-gray-100 text-black dark:bg-gray-800 dark:text-white rounded hover:bg-gray-200 dark:hover:bg-gray-700 transition text-2xl"
-      onClick={toggleTheme}
-      title="Toggle Theme"
-    >
-      {themeName === 'dark' ? '🌙' : '☀️'}
-    </button>
+      <button 
+        className="px-4 py-2 bg-gray-100 text-black dark:bg-gray-800 dark:text-white rounded hover:bg-gray-200 dark:hover:bg-gray-700 transition text-2xl"
+        onClick={toggleTheme}
+        title="Toggle Theme"
+      >
+        {themeName === 'dark' ? '🌙' : '☀️'}
+      </button>
+    </div>
   </nav>
 
       <main className="flex-1 p-4">
